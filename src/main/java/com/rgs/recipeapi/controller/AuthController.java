361856df94a95b41a8ec1b7ccbd11e6b51b2513a
Evan-xma <xma.evan@gmail.com>
2025-12-24 20:57:57 +0000

    fix: API returns wrong data
    Jira: RCPAPI-14

---
+++ src/main/java/com/rgs/recipeapi/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.rgs.recipeapi.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public Map<String, String> login() {
+        return Map.of("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.dozjgNryP4J3jVmNHl0w5N_XgL0n3I9PlFUP0THsR8U");
+    }
+}
